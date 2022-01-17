9dbccb41c7f750398a9df9aaeec8fa4934b1c36f
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-01-17 17:38:05 +0530

    assigment 1 added

---
+++ Assignment/ActionResult_Assignment/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResult_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string input)
+        {
+    
+            if (input == "sample")
+            {
+                return File("~/Sample.pdf", "application/pdf");
+            }
+            else if (input == "gotoabout")
+            {
+                return RedirectToAction("About");
+            }
+
+            else if (input == "login")
+            {
+                return View("Login");
+            }
+            else
+            {
+                return Content("You entered value" + input);
+            }
+        }
+
+        // GET: Home/About
+        public ActionResult About()
+        {
+            return Content("about action method content");
+        }
+    }
+}
