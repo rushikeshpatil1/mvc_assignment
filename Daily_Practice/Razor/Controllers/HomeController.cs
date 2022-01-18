ee65a1430211cf01bd466b42269945d8ca23701a
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-01-18 15:46:33 +0530

    day 2

---
+++ Daily_Practice/Razor/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.no = 1;
+            ViewBag.name = "rushikesh";
+            ViewBag.marks = 60;
+            ViewBag.Subjects = 6;
+            ViewBag.Remarks = new List<String>() {"A","B","A+" };
+
+
+            return View();
+        }
+    }
+}
