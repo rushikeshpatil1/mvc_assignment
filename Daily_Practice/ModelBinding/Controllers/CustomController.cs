47b35f94ee313687c91e40a52c4c33f8ab2d14ee
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-01-24 10:59:08 +0530

    day 5

---
+++ Daily_Practice/ModelBinding/Controllers/CustomController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBinding.Models;
+namespace ModelBinding.Controllers
+{
+    public class CustomController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([ModelBinder(typeof(customBind))] Class1 c)
+        {
+            return View();
+        }
+    }
+}
---
+++ Daily_Practice/ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBinding.Models;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+   /*     [HttpPost]
+        public ActionResult Create(Class1 c)
+        {
+            return View();
+        }*/
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "id")] Class1 c)
+        {
+            return View();
+        }
+    }
+}
