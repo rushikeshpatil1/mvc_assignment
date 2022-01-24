96de25933fa39ac44aeedeb4582c2e7a04409911
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-01-24 10:56:34 +0530

    day 5

---
+++ Daily_Practice/Models/Models/model2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models.Models
+{
+    public class model2
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
