f64a7185129c85e823177dea6cd05fbb7cb46476
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-01-27 12:55:52 +0530

    day 8

---
+++ Daily_Practice/HtmlHelper/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HtmlHelpersEx.Models;
+
+namespace HtmlHelpersEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            model1 m = new model1();
+            List<model1> l = new List<model1>() {
+            new model1(){ id=1,name="Rushikesh"},
+            new model1(){ id=2,name="Rp"}
+            };
+            
+            return View(l);
+        }
+    }
+}
