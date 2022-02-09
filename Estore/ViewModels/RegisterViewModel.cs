7108e85d4589c4a30693d8b89b315d2988585be0
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-02-09 12:30:55 +0530

    updated assignment

---
+++ Estore/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Estore.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Username can't be blank")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password can't be blank")]
+        [Compare("Password", ErrorMessage = "Password and confirm password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string email { get; set; }
+
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+
+    }
+}
