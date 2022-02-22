34741f1961198187fd9bf08d67646ba9ce91c23c
Rushikesh Patil <rushikeshpatil@benchmarkit.solutions>
2022-02-22 10:10:20 +0530

    Dot net app

--- dotNetCore/client/src/app/app.component.ts
+++ dotNetCore/client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'client';
+  
+  constructor(private http: HttpClient){}
+users:any;
+
+  ngOnInit()  {
+  this.getUsers();
+    
+  }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(Response => {
+        this.users=Response
+      },error => {console.log(error)
+      });
+  }
 }
+
+
